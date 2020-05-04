c4d099419186eb3fea4388bc72385a5ec1d9bd08
ericknegreter <erickxon-roy@hotmail.com>
2020-05-04 12:13:09 -0500

    actualice el script para un mejor performance

--- install_sh.sh
+++ install_sh.sh
@@ -8,9 +8,9 @@ apt-get install fswebcam -y
 apt-get install git-core -y
 apt-get install xterm -y
 sudo apt-get install python-dev default-libmysqlclient-dev -y
-sudo apt-get install python3-dev
-sudo apt-get install python3-mysql.connector
-sudo apt-get install flac
+sudo apt-get install python3-dev -y
+sudo apt-get install python3-mysql.connector -y
+sudo apt-get install flac -y
 sudo apt-get install portaudio19-dev -y
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
@@ -22,17 +22,17 @@ echo "Instalacion de librerias Python3"
 sudo -H pip3 install SpeechRecognition
 sudo -H pip3 install unidecode
 sudo -H pip3 install paramiko
-sudo -H pip3 install cryptography==2.4.2
+sudo -H pip3 install cryptography
 sudo -H pip3 install mysql-connector-python
 sudo -H pip3 install pyaudio
 echo "Instalacion de scripts"
-echo '#!/usr/bin/python3.5
+echo '#!/usr/bin/python3.7
 import time
 import sys
 import mysql.connector
 from datetime import date, datetime
 peticion=sys.argv[1]
-cnx = mysql.connector.connect(user="LMV_ADMIN", password="LABORATORIOT4", host="10.0.5.246", database='LMV')
+cnx = mysql.connector.connect(user="LMV_ADMIN", password="MINIMOT4", host="10.0.5.246", database='LMV')
 cursor = cnx.cursor(buffered=True)
 today = datetime.now()
 print(today)
